6f2d7a21dd24f93e2e639bd2bb8c3c8914a5a0ac
Hisataka-Hagiyama <hisataka7@icloud.com>
2023-02-21 20:24:27 +0900

    Solve Problem3 and Add gitignore

---
+++ Problem3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 素数を計算する
+
+int main(void)
+{
+    long long num = 600851475143;
+    long long num2 = num;
+    long long ans = 0;
+    // 素因数分解を行う
+    for (long long i = 2; i * i <= num; i++)
+    {
+        if (num2 % i == 0)
+        {
+            ans = max(ans, i); // 素因数の最大値を更新
+            while (num2 % i == 0)
+            {
+                num2 /= i;
+            }
+        }
+    }
+    cout << ans << endl;
+}
